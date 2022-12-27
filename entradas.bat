efd9e8778745cfb784b068992c74bb153c25a753
RFChanchay <63420638+RFChanchay@users.noreply.github.com>
2022-12-27 14:09:35 -0500

    Add files via upload

---
+++ entradas.bat
@@ -0,0 +1,18 @@
+set url=https://www.ticketfacil.ec/
+START "" chrome.exe "--profile-directory=Profile 2" %url%
+START "" chrome.exe "--profile-directory=Profile 3" %url%
+START "" chrome.exe "--profile-directory=Profile 4" %url%
+START "" chrome.exe "--profile-directory=Profile 5" %url%
+START "" chrome.exe "--profile-directory=Profile 6" %url%
+START "" chrome.exe "--profile-directory=Profile 7" %url%
+START "" chrome.exe "--profile-directory=Profile 8" %url%
+START "" chrome.exe "--profile-directory=Profile 9" %url%
+START "" chrome.exe "--profile-directory=Profile 10" %url%
+START "" chrome.exe "--profile-directory=Profile 11" %url%
+START "" chrome.exe "--profile-directory=Profile 12" %url%
+START "" chrome.exe "--profile-directory=Profile 13" %url%
+START "" chrome.exe "--profile-directory=Profile 14" %url%
+START "" chrome.exe "--profile-directory=Profile 15" %url%
+START "" chrome.exe "--profile-directory=Profile 16" %url%
+START "" chrome.exe "--profile-directory=Profile 17" %url%
+
